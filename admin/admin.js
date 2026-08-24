ad84e167160e5f7c477304a237762842933fb100
satyamrai-2005 <satyamkmr7993@gmail.com>
2026-08-24 17:52:58 +0530

    Update Supabase URL to remove endpoint path

--- admin/admin.js
+++ admin/admin.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL ="https://mpafolafsarugztvdcfa.supabase.co/rest/v1/";
+const SUPABASE_URL ="https://mpafolafsarugztvdcfa.supabase.co";
 const SUPABASE_KEY ="sb_publishable_G_GWDzMhx2AT1HbCNepPDA_b8UY9jQN";
 
 const supabaseClient = window.supabase.createClient(
